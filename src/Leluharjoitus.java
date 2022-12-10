6542c8c3f6ae48b22aee1e6b7efcf02db47c4736
Jouko-L <jouko.lipponen@student.hamk.fi>
2022-12-10 20:23:29 +0200

    päivitys
    koodia ja kommentteja

--- src/Leluharjoitus.java
+++ src/Leluharjoitus.java
@@ -2,16 +2,19 @@
 public class Leluharjoitus {
 
 	public static void main(String[] args) {
-		Lelu lelu = new Lelu("auto",5);
+		Lelu lelu = new Lelu("nukke",5);				//Luodaan oliot
 		lelu.tulostin();
-		Lelu lelu1 = new Lelu("vieteriauto", 100);
-		lelu1.tulostin();
+		Vieteriauto vauto = new Vieteriauto("Vieteriauto",10);
+		vauto.tulostin();
+		vauto.kiihdyta();
+		vauto.kiihdyta();
+		vauto.kiihdyta();
 		
-	}
+	}//Mainin lopetus
 }
 	class Lelu {
-		private String nimi;
-		private int paino;
+		 String nimi;
+		 int paino;
 		
 	public Lelu() {
 		nimi = "";
@@ -26,25 +29,31 @@ public class Leluharjoitus {
 		System.out.println(paino);
 	}
 	
-	}
-	class Vieteriauto extends Lelu {
+	}	//Lelu luokan loppu
+	
+	class Vieteriauto extends Lelu {	//Periytyvä luokka
 		
-		int nopeus;
+		private int nopeus;
 		
 	public Vieteriauto() {
-		
+		nimi = "";
+		paino = 0;
 		nopeus = 0;
-		
+				
 	}
-	public Vieteriauto(int speed) {
-		nopeus = speed;
+	public Vieteriauto(String nimi, int paino) {
+		this.nimi = nimi;
+		this.paino = paino;
+		nopeus = 0;
 	}
 	void kiihdyta() {
 		nopeus++;
+		System.out.println("Auto kiihtyy, vauhti on: " +nopeus);
 	}
 	@Override
 	void tulostin() {
 		super.tulostin();
+		System.out.println(nopeus);
 	}
 
 }
