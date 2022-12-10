563a5e560cf5919c80787705a2bdd618786d6970
Jouko-L <jouko.lipponen@student.hamk.fi>
2022-12-10 18:50:36 +0200

    testi

---
+++ src/Leluharjoitus.java
@@ -0,0 +1,30 @@
+
+public class Leluharjoitus {
+
+	public static void main(String[] args) {
+		// testiä
+
+	}
+	class Lelu1 {
+		String nimi;
+		int paino;
+		
+	public Lelu1() {
+		nimi = "";
+		paino = 0;
+	}
+	public Lelu1(String name, int weight) {
+		nimi = name;
+		paino = weight;
+	}
+	void tulostin() {
+		System.out.println(nimi);
+		System.out.println(paino);
+	}
+	void mustikkamehu() {
+		
+	}
+	}
+	
+
+}
